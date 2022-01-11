52f68a38e9dd59739491c5a66720b20725932d2d
agolautner <lautner.agoston@gmail.com>
2022-01-11 13:30:20 +0100

    added changeValue function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
   let boolean = false;
+
+  const changeValue = () => {
+    console.log(boolean);
+    boolean = !boolean;
+    console.log(boolean);
+  }
+
   return (
     <div className="App">
       <p>Hello World!</p>
       <p>{boolean ? 'the boolean is true' : 'the boolean is false'}</p>
+      <button onClick={changeValue}>CLICK ME</button>
     </div>
   );
 }
