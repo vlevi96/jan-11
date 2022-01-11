04be8e66094e220437ca169d7800e972565e78fa
agolautner <lautner.agoston@gmail.com>
2022-01-11 14:17:27 +0100

    added some console logs for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 function App() {
-  let boolean = false;
+  let boolean = true;
+  let content;
 
   const changeValue = () => {
-    console.log(boolean);
+    console.log('changing boolean from ', boolean);
     boolean = !boolean;
-    console.log(boolean);
+    console.log('boolean is now ', boolean);
+  }
+
+  if (boolean === false) {
+    content = 'the boolean is false'
+  } else if (boolean === true){
+    content = 'the boolean is true'
+  } else {
+    content = 'the variable called boolean is not actually a boolean value :('
   }
 
   return (
     <div className="App">
       <p>Hello World!</p>
-      <p>{boolean ? 'the boolean is true' : 'the boolean is false'}</p>
+      {/* <p>{boolean ? 'the boolean is true' : 'the boolean is false'}</p> */}
+      <p>{content}</p>
       <button onClick={changeValue}>CLICK ME</button>
     </div>
   );
