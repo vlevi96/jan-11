964dc3360f89dd0b8e2e36b30c3df5f42b32322c
agolautner <lautner.agoston@gmail.com>
2022-01-11 14:30:56 +0100

    implemented useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
+import { useState } from 'react';
+
 function App() {
-  let boolean = true;
-  let content;
-
+  // let boolean = true;
+  let [boolean, setBoolean] = useState(true);
+  
   const changeValue = () => {
-    console.log('changing boolean from ', boolean);
-    boolean = !boolean;
-    console.log('boolean is now ', boolean);
+    setBoolean(!boolean);
+    // console.log('changing boolean from ', boolean);
+    // boolean = !boolean;
+    // console.log('boolean is now ', boolean);
   }
+  
+  // let content;
 
-  if (boolean === false) {
-    content = 'the boolean is false'
-  } else if (boolean === true){
-    content = 'the boolean is true'
-  } else {
-    content = 'the variable called boolean is not actually a boolean value :('
-  }
+  // if (boolean === false) {
+  //   content = 'the boolean is false'
+  // } else if (boolean === true){
+  //   content = 'the boolean is true'
+  // } else {
+  //   content = 'the variable called boolean is not actually a boolean value :('
+  // }
 
   return (
     <div className="App">
       <p>Hello World!</p>
-      {/* <p>{boolean ? 'the boolean is true' : 'the boolean is false'}</p> */}
-      <p>{content}</p>
+      {/* <p>{content}</p> */}
+      <p>{boolean ? 'the boolean is true' : 'the boolean is false'}</p>
       <button onClick={changeValue}>CLICK ME</button>
     </div>
   );
