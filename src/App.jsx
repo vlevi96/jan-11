56dc817e9dc2d542d7f6739e9b42e17ab447717b
vlevi96 <vlevi9609@gmail.com>
2022-01-12 15:20:04 +0100

    Szerdai óra

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import { useState } from 'react';
+const cars = [
+  {brand: "Toyota", model: "yaris", topSpeed: "190km/h"},
+  {brand: "Mercedes", model: "C63", topSpeed: "260km/h"},
+  {brand: "Mazda", model: "6", topSpeed: "210km/h"},
+  {brand: "Mazda", model: "Civic", topSpeed: "200km/h"},
+  {brand: "Audi", model: "A4", topSpeed: "220km/h"},
+]
 
 function App() {
   // let boolean = true;
@@ -24,8 +31,17 @@ function App() {
   // let pageName = 'Home';
   let [pageName, setPageName] = useState('Landing');
 
+  const [value,setValue]=useState("");
+  console.log("rerender");
+  console.log(value);
+  // const transformToDiv=(carObject)=> <div>{carObject.brand} - {carObject.model}({carObject.topSpeed})</div>;
+  // const transformToX=(carObject)=>
+  // {
+  //   return "x";
+  // }
   return (
     <div className="App">
+    
       <div>
         <p>Hello World!</p>
         {/* <p>{content}</p> */}
@@ -42,7 +58,18 @@ function App() {
       {pageName === 'Home' && <div>text here</div>}
       {pageName === 'About' && <div><h1>text here</h1></div>}
       {pageName === 'Landing' && <div><input type="text" placeholder='input' /></div>}
+      
+      <hr />
+
+      <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} />
+      {/* {value.length >=3 ? <button>Submit</button> : <button disabled>Submit</button>} */}
+      <button disabled={value.length<3}>Submit</button>
+      {value.length <3  && <p>Min. 3 char!</p>}
+
+      {cars.filter(car=>car.brand.startsWith(value)).map(carObject=> <div key={carObject.model}>{carObject.brand} - {carObject.model}({carObject.topSpeed})</div>)}
     </div>
+
+  
   );
 }
 
